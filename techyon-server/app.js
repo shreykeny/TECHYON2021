1265af524ac0aefa6d5346ab6ba7ff8c025e1955
amarnarute <amarnarute2002@gmail.com>
2021-09-08 23:35:13 +0530

    Changed mongoDB connection string

--- techyon-server/app.js
+++ techyon-server/app.js
@@ -1,35 +1,31 @@
-const express =require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const public = require("./Routes/public")
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const public = require('./Routes/public');
 dotenv.config();
 var bodyParser = require('body-parser');
 
-
-const teams =  require("./Routes/PostTeams")
-const events =  require("./Routes/PostEvents")
+const teams = require('./Routes/PostTeams');
+const events = require('./Routes/PostEvents');
 //connect to DB
-mongoose.connect(process.env.DB_CONNECT,
-{ useNewUrlParser: true,
-    
-},
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
 
-()=>{
-
-    console.log('connected to DB')
-}
-)
+  () => {
+    console.log('connected to DB');
+  }
+);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.use("/event", events)
-app.use("/team", teams)
-app.use("/", public)
+app.use('/event', events);
+app.use('/team', teams);
+app.use('/', public);
 
-
-app.listen(3000, () =>{
-    console.log('app is running ')
-})
+app.listen(3000, () => {
+  console.log('app is running ');
+});
