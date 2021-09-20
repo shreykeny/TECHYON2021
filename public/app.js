acaeb87351ff2b5cb31b222bcfce7d856679c560
abdullah nore <49518940+abdullahnore@users.noreply.github.com>
2021-09-20 18:07:15 +0530

    18 sep
    filter ,added pading ,nav changes and sponsors individual cards

--- public/app.js
+++ public/app.js
@@ -47,3 +47,50 @@ $('.smooth').on('click',function(event)
   );
 }});
 //ENDD
+
+// FILTER CODE
+function  filterObject(c)
+{  
+  var x ,i;
+  x= document.getElementsByClassName("cardE");
+  if(c=="all")
+  { c="";  
+} else {
+  
+}
+ 
+  for(i=0;i<x.length;i++)
+  {
+    removeClass(x[i],"block");
+    removeClass(x[i],"hidden");
+    if(x[i].className.indexOf(c)>-1) 
+    { addClass(x[i],"block"); 
+         
+  } else {
+    addClass(x[i],"hidden");
+  }
+  }
+}
+function addClass(element,name)
+{
+  var i,arr1,arr2;
+  arr1= element.className.split(" ");
+  arr2=name.split(" ");
+  for(i=0 ;i<arr2.length;i++)
+  { if(arr1.indexOf(arr2[i])==-1)
+   {
+     element.className += " " + arr2[i];
+   }
+  }
+}
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
